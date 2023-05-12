ac7150ca75aee259c57e90e41da170592ffd1ce4
Pai <pl9ed15@gmail.com>
2023-05-12 01:06:32 -0400

    Use env token

--- src/main/kotlin/com/tubefans/gamepicker/GamePickerApplication.kt
+++ src/main/kotlin/com/tubefans/gamepicker/GamePickerApplication.kt
@@ -4,7 +4,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories
 
-const val TEST_TOKEN = "MTEwNjEyNjY2MjkzMzk1NDU4MQ.GrV-zz.S7bHFH9wSIcVDx38F0vueBEtnvpESGWqGr7NAQ"
+val TEST_TOKEN = System.getenv()["TEST_BOT_TOKEN"]
 
 @EnableMongoRepositories
 @SpringBootApplication
