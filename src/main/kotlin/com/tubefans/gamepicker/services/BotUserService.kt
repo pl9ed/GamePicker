7d841e17c653a4e44a7ae62ec6dc3a94ac94375f
Pai <pl9ed15@gmail.com>
2023-05-14 07:36:22 -0400

    First pass getUsersFromName()

--- src/main/kotlin/com/tubefans/gamepicker/services/BotUserService.kt
+++ src/main/kotlin/com/tubefans/gamepicker/services/BotUserService.kt
@@ -3,8 +3,17 @@ package com.tubefans.gamepicker.services
 import com.tubefans.gamepicker.dto.BotUser
 import com.tubefans.gamepicker.extensions.updateGame
 import com.tubefans.gamepicker.repositories.BotUserRepository
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.reactor.mono
+import kotlinx.coroutines.runBlocking
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
+import reactor.core.publisher.Mono
 
 @Service
 class BotUserService @Autowired constructor(
@@ -20,4 +29,20 @@ class BotUserService @Autowired constructor(
 
     fun updateGameForUserWithId(id: String, game: String, score: Long): BotUser =
         updateUser(botUserRepository.findById(id).get().updateGame(game, score))
+
+    fun getUsersFromNames(names: Collection<String>) = runBlocking {
+        val userSet = mutableSetOf<BotUser>()
+        val failedSet = mutableSetOf<String>()
+        names.map { name ->
+            async {
+                try {
+                    userSet.add(botUserRepository.findOneByName(name).get())
+                } catch (e: NoSuchElementException) {
+                    failedSet.add(name)
+                }
+            }
+        }.awaitAll()
+
+        return@runBlocking listOf(userSet, failedSet)
+    }
 }
