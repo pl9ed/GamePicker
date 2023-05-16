3a6d72875d8840eae090b74a49528cc96c177085
Pai <pl9ed15@gmail.com>
2023-05-16 19:56:29 -0400

    Removed unused create_event and replaced it with a message

--- src/main/kotlin/com/tubefans/gamepicker/commands/CreateEventCommand.kt
+++ src/main/kotlin/com/tubefans/gamepicker/commands/CreateEventCommand.kt
@@ -12,34 +12,10 @@ import org.springframework.stereotype.Component
 @Component
 class CreateEventCommand : SlashCommand {
 
-    @Autowired
-    lateinit var gateway: GatewayDiscordClient
-
     override val name = "create_event"
 
     override fun handle(event: ChatInputInteractionEvent): InteractionApplicationCommandCallbackReplyMono {
         // TODO: implement
-        var channelName: String? = null
-        val usersInChannel = mutableListOf<String>()
-
-        mono {
-            gateway.guilds.filter { it.id == Snowflake.of(1106152112557146152) }
-                .flatMap { guild ->
-                    guild.channels.filter { channel ->
-                        channel.id == Snowflake.of(1106152113001730091)
-                    }
-                }.flatMap {
-                    channelName = it.name
-                    (it as VoiceChannel).voiceStates
-                }.flatMap {
-                    it.user
-                }.map {
-                    it.username
-                }.subscribe {
-                    usersInChannel.add(it)
-                }
-        }.block()
-
-        return event.reply().withEphemeral(false).withContent("$channelName has ${usersInChannel.joinToString()}")
+        return event.reply().withEphemeral(false).withContent("Events still under development")
     }
 }
