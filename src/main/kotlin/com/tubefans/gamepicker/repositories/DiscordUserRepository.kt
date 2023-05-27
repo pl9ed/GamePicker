aca99060df013c4b2f7596eb63ac90a27e3dacc1
Pai <pl9ed15@gmail.com>
2023-05-27 04:01:44 -0400

    Removed @Repository annotation

--- src/main/kotlin/com/tubefans/gamepicker/repositories/DiscordUserRepository.kt
+++ src/main/kotlin/com/tubefans/gamepicker/repositories/DiscordUserRepository.kt
@@ -5,7 +5,6 @@ import org.springframework.data.mongodb.repository.MongoRepository
 import org.springframework.stereotype.Repository
 import java.util.Optional
 
-@Repository
 interface DiscordUserRepository : MongoRepository<DiscordUser, String> {
     fun findOneByName(name: String): Optional<DiscordUser>
 }
