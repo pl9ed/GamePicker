6a1fe9039c55d3886884e7315a107bfe92425ea2
Pai <pl9ed15@gmail.com>
2023-05-27 08:02:31 -0400

    test afterPropertiesSet

--- src/main/kotlin/com/tubefans/gamepicker/TestCache.kt
+++ src/main/kotlin/com/tubefans/gamepicker/TestCache.kt
@@ -3,18 +3,18 @@ package com.tubefans.gamepicker
 import com.tubefans.gamepicker.dto.DiscordUser
 import com.tubefans.gamepicker.repositories.DiscordUserRepository
 import javax.annotation.PostConstruct
+import org.springframework.beans.factory.InitializingBean
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Component
 
 @Component
 class TestCache @Autowired constructor(
     private val discordUserRepository: DiscordUserRepository
-) {
+) : InitializingBean {
 
     private val users = mutableSetOf<DiscordUser>()
 
-    @PostConstruct
-    fun init() {
+    override fun afterPropertiesSet() {
         users.add(discordUserRepository.findOneByName("ANDREW").get())
     }
 }
