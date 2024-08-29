ec0c35ffb81a62c92c16a011e558dc2df93fa070
Pai <pl9ed15@gmail.com>
2024-08-29 15:46:45 -0400

    Init arbitragexiv module

---
+++ arbitragexiv/build.gradle.kts
@@ -0,0 +1,20 @@
+plugins {
+    id("java")
+    kotlin("jvm") version "2.0.0"
+}
+
+group = "com.tubefans.gamepicker"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -23,3 +23,4 @@ dependencyResolutionManagement {
 
 include("spring-application")
 include("core")
+include("arbitragexiv")
