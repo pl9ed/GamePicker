1417b1ab83ddc34be67322c641b6a09a470369c0
Pai <pl9ed15@gmail.com>
2024-08-31 06:53:12 -0400

    Updated google-sheets-persistence to use version catalog

--- persistence/google-sheets-persistence/build.gradle.kts
+++ persistence/google-sheets-persistence/build.gradle.kts
@@ -13,18 +13,7 @@ dependencies {
     implementation(project(":core"))
     implementation(project(":persistence:ports"))
 
-    api("com.google.api-client:google-api-client:1.25.0")
-    api("com.google.apis:google-api-services-sheets:v4-rev612-1.25.0")
-    api("com.google.apis:google-api-services-drive:v3-rev197-1.25.0")
-
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
-    implementation(kotlin("stdlib-jdk8"))
-}
-
-tasks.test {
-    useJUnitPlatform()
-}
-kotlin {
-    jvmToolchain(17)
+    api(googleLibs.apiClient)
+    api(googleLibs.apiServicesSheets)
+    api(googleLibs.drive)
 }
