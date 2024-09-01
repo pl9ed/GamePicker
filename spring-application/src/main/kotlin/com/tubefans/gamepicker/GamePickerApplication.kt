1cd0318831c310a5d148da5b0ab983861a046697
Pai <pl9ed15@gmail.com>
2024-09-01 05:12:50 -0400

    Added @EnableConfigurationProperties and @ComponentScan to base spring application
    Fixed randomized test setup

--- spring-application/src/main/kotlin/com/tubefans/gamepicker/GamePickerApplication.kt
+++ spring-application/src/main/kotlin/com/tubefans/gamepicker/GamePickerApplication.kt
@@ -1,12 +1,16 @@
 package com.tubefans.gamepicker
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.ComponentScan
 
 const val PROJECT_NAME = "GamePicker"
 const val APP_NAME = PROJECT_NAME
 
 @SpringBootApplication
+@EnableConfigurationProperties
+@ComponentScan(basePackages = ["com.tubefans.gamepicker", "com.tubefans.arbitragexiv"])
 class GamePickerApplication
 
 fun main(args: Array<String>) {
