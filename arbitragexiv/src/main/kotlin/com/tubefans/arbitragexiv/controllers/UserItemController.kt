2bc3cfbfb1304104f36d1688ae2a31c989c93838
Pai <pl9ed15@gmail.com>
2024-09-01 06:34:10 -0400

    Added logs to controller endpoints

--- arbitragexiv/src/main/kotlin/com/tubefans/arbitragexiv/controllers/UserItemController.kt
+++ arbitragexiv/src/main/kotlin/com/tubefans/arbitragexiv/controllers/UserItemController.kt
@@ -2,23 +2,36 @@ package com.tubefans.arbitragexiv.controllers
 
 import com.tubefans.arbitragexiv.dto.AddItemRequest
 import com.tubefans.arbitragexiv.services.UserItemListService
-import org.springframework.stereotype.Controller
+import com.tubefans.gamepicker.models.UserItemList
+import org.slf4j.LoggerFactory
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import reactor.core.publisher.Mono
 
-@Controller
+@RestController
+@RequestMapping("/arbitragexiv/items")
 class UserItemController(
     private val userItemListService: UserItemListService,
 ) {
-    @GetMapping("/arbitragexiv/{userId}/items")
+    private val log = LoggerFactory.getLogger(this::class.java)
+
+    @GetMapping("/{userId}")
     fun getItems(
         @PathVariable("userId") userId: String,
-    ) = userItemListService.getUserItemList(userId)
+    ): Mono<UserItemList> {
+        log.info("Getting items for user $userId")
+        return userItemListService.getUserItemList(userId)
+    }
 
-    @PostMapping("/arbitragexiv/items")
+    @PostMapping("/add")
     fun addItems(
         @RequestBody addRequest: AddItemRequest,
-    ) = userItemListService.addItem(addRequest)
+    ): Mono<UserItemList> {
+        log.info("Adding items for user ${addRequest.userId}, request: $addRequest")
+        return userItemListService.addItem(addRequest)
+    }
 }
