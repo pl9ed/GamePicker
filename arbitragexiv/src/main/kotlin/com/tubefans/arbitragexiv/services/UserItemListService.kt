d7ef498e994734ff28f12a8ba91c4407b6c5ecbd
Pai <pl9ed15@gmail.com>
2024-09-02 14:07:32 -0400

    Return NotFoundException when findById fails for GET

--- arbitragexiv/src/main/kotlin/com/tubefans/arbitragexiv/services/UserItemListService.kt
+++ arbitragexiv/src/main/kotlin/com/tubefans/arbitragexiv/services/UserItemListService.kt
@@ -3,15 +3,20 @@ package com.tubefans.arbitragexiv.services
 import com.tubefans.arbitragexiv.dao.UserItemList
 import com.tubefans.arbitragexiv.dto.AddItemRequest
 import com.tubefans.arbitragexiv.dto.RemoveItemRequest
+import com.tubefans.arbitragexiv.exceptions.NotFoundException
 import com.tubefans.arbitragexiv.repositories.UserItemListRepository
 import org.springframework.stereotype.Service
 import reactor.core.publisher.Mono
+import reactor.kotlin.core.publisher.switchIfEmpty
 
 @Service
 class UserItemListService(
     private val userItemListRepository: UserItemListRepository,
 ) {
-    fun getUserItemList(userId: String): Mono<UserItemList> = userItemListRepository.findById(userId)
+    fun getUserItemList(userId: String): Mono<UserItemList> =
+        userItemListRepository
+            .findById(userId)
+            .switchIfEmpty { Mono.error(NotFoundException("No items found for $userId")) }
 
     fun addItem(request: AddItemRequest): Mono<UserItemList> =
         userItemListRepository
---
+++ core/arbitragexiv/src/main/kotlin/com/tubefans/arbitragexiv/exceptions/NotFoundException.kt
@@ -0,0 +1,8 @@
+package com.tubefans.arbitragexiv.exceptions
+
+class NotFoundException : RuntimeException {
+    constructor(message: String) : super(message)
+    constructor(message: String, cause: Throwable) : super(message, cause)
+    constructor(cause: Throwable) : super(cause)
+    constructor() : super()
+}
