6717d7275c6aeaf6966d408da510e87fad0992d8
Pai <pl9ed15@gmail.com>
2024-09-02 11:58:41 -0400

    Added @JsonProperty annotation to AddItemRequest

--- arbitragexiv/src/main/kotlin/com/tubefans/arbitragexiv/dto/AddItemRequest.kt
+++ arbitragexiv/src/main/kotlin/com/tubefans/arbitragexiv/dto/AddItemRequest.kt
@@ -1,8 +1,9 @@
 package com.tubefans.arbitragexiv.dto
 
+import com.fasterxml.jackson.annotation.JsonProperty
 import com.tubefans.arbitragexiv.models.ListType
 
 data class AddItemRequest(
-    val userId: String,
-    val additionalItems: Map<ListType, List<Int>>,
+    @JsonProperty("user_id") val userId: String,
+    @JsonProperty("additional_items") val additionalItems: Map<ListType, List<Int>>,
 )
